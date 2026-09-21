3a7eb0d0a31e7d39139cbe242d35fb85305f5fec
BYRON-lang <byronkennedy22@gmail.com>
2026-09-21 14:41:19 +0200

    feat(connectors): brand svg icons + ConnectorIcon component

---
+++ src/components/ConnectorIcon.tsx
@@ -0,0 +1,53 @@
+import discord from "../assets/connectors/discord.svg?raw";
+import figma from "../assets/connectors/figma.svg?raw";
+import github from "../assets/connectors/github.svg?raw";
+import gitlab from "../assets/connectors/gitlab.svg?raw";
+import google from "../assets/connectors/google.svg?raw";
+import homeassistant from "../assets/connectors/homeassistant.svg?raw";
+import linear from "../assets/connectors/linear.svg?raw";
+import notion from "../assets/connectors/notion.svg?raw";
+import slack from "../assets/connectors/slack.svg?raw";
+import spotify from "../assets/connectors/spotify.svg?raw";
+import telegram from "../assets/connectors/telegram.svg?raw";
+import todoist from "../assets/connectors/todoist.svg?raw";
+import trello from "../assets/connectors/trello.svg?raw";
+
+const ICONS: Record<string, string> = {
+  discord,
+  figma,
+  github,
+  gitlab,
+  google,
+  ha: homeassistant,
+  linear,
+  notion,
+  slack,
+  spotify,
+  telegram,
+  todoist,
+  trello,
+};
+
+type Props = {
+  id: string;
+  size?: number;
+  className?: string;
+};
+
+export default function ConnectorIcon({ id, size = 20, className }: Props) {
+  const svg = ICONS[id];
+  if (!svg) return null;
+
+  return (
+    <span
+      className={"inline-flex shrink-0 items-center justify-center " + (className ?? "")}
+      style={{ width: size, height: size }}
+      dangerouslySetInnerHTML={{
+        __html: svg.replace(
+          "<svg ",
+          `<svg width="${size}" height="${size}" style="display:block" `,
+        ),
+      }}
+    />
+  );
+}
